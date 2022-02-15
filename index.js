903d4789501f91b0139a9e6c090979daddb922d7
Jefferson SA <jeff_zb@msn.com>
2022-02-15 16:19:11 -0300

    Criando conexao com o banco de dados

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const configExpress = require("./config/configExpress");
+const conexao = require("./infra/conexao");
+
+conexao.connect(erro => {
+    if(erro) {
+        console.log(erro)
+    } else {
+        console.log('conectado com sucesso')
+    }
+});
 
 const app = configExpress();

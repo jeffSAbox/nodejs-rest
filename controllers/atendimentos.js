8672c307911cabc174f4fc5b50b2d785e6a04823
Jefferson SA <jeff_zb@msn.com>
2022-02-15 17:05:48 -0300

    criado o models e a persistencia ao banco de dados

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,14 @@
+const Atendimento = require("../models/atendimento");
+
 module.exports = app => {
+
   app.get("/atendimentos", (req, res) => res.send("rota de atendimentos"));
 
-
   app.post("/atendimentos", (req, res) => {
-    console.log(req.body);
+
+    const atendimento = req.body;
+    Atendimento.adicionar(atendimento);
+
     res.send("roda de atendimentos via POST")
 
   });

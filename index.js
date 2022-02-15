5c63409ff94096728633cbe3fd5cba7f9ab1e0e7
Jefferson SA <jeff_zb@msn.com>
2022-02-15 16:58:40 -0300

    Infra tabelas: criador de tabela atendimentos no banco de dados

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const configExpress = require("./config/configExpress");
 const conexao = require("./infra/conexao");
+const tabelas = require("./infra/tabelas");
 
 conexao.connect(erro => {
     if(erro) {
@@ -7,6 +8,7 @@ conexao.connect(erro => {
     } else {
         console.log('conectado com sucesso')
 
+        tabelas.init(conexao);
         const app = configExpress();
 
         app.listen(3000, () => {console.log('Servidor rodando...')});

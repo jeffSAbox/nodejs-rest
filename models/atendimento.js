495620aa0441296ab49fce97041e76ca35c8da2a
Jefferson SA <jeff_zb@msn.com>
2022-02-18 14:54:05 -0300

    Incluindo 2 colunas(data e dataCriacao) no insert atendimentos

--- models/atendimento.js
+++ models/atendimento.js
@@ -1,12 +1,16 @@
 const conexao = require("../infra/conexao");
+const moment = require("moment");
 
 class Atendimento{
 
   adicionar(atendimento){
 
-    const sql = 'INSERT INTO Atendimentos SET ?'
+    let sql = 'INSERT INTO Atendimentos SET ?'
+    let dataCriacao = new Date();
+    let data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
+    let arr_atendimento = {...atendimento, dataCriacao, data};
 
-    conexao.query(sql, atendimento, (erro, resultados) => {
+    conexao.query(sql, arr_atendimento, (erro, resultados) => {
         if(erro) {
             console.log(erro)
         } else {

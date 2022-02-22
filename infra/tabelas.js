fbdae871d9dae512bbf0af2fc14b91a4683320fb
Jeff S.A <jeff_zb@msn.com>
2022-02-22 15:39:29 -0300

    Alteração da msg 'tabela criada' ao iniciar o server

--- infra/tabelas.js
+++ infra/tabelas.js
@@ -9,11 +9,24 @@ class tabelas{
   criarAtendimentos(){
     const sql = "CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))"
 
-    this.conexao.query(sql, erro => {
-      if(erro) {
+    this.conexao.query(sql, (erro, resposta) => {
+      if(erro) 
+      {
+
           console.log(erro)
-      } else {
-          console.log('Tabela Atendimentos criada com sucesso')
+
+      } else 
+      {
+        
+        if( resposta.warningStatus == 1 )
+        {
+          console.log('Tabela Atendimentos já existe');
+        }
+        else
+        {
+          console.log('Tabela Atendimentos criada com sucesso');
+        }
+
       }
     })
   }
